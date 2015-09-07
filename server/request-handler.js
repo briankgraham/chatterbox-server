27b1de1b1f8ab77da2b57259fbffafd7ef6c5e1d
Hack Reactor Students <accounts@hackreactor.com>
2015-09-07 16:13:20 -0700

    Initial commit

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+module.exports = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -28,7 +28,7 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
-
+  
   // The outgoing status.
   var statusCode = 200;
 
@@ -39,11 +39,19 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
-
+  headers['Content-Type'] = "application/json";
+  if (request.method === 'GET' && request.url === '/log') {
+    response.writeHead(statusCode, headers);  
+  }
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    response.writeHead(201, headers);
+  } 
+  else {
+    response.writeHead(statusCode, headers);  
+  }
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +60,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+
+  response.end();
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
